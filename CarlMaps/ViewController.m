43bbda29fe331042b1c160bb37e87361abc7a667
MobileDev <mobiledev@cmc307-04.mathcs.carleton.edu>
2014-05-12 11:53:35 -0500

    Made the map start over Carleton’s Campus by default and restricted zoom.

--- CarlMaps/ViewController.m
+++ CarlMaps/ViewController.m
@@ -22,20 +22,24 @@
 {
     
     // Create a GMSCameraPosition that tells the map to display the
-    // coordinate -33.86,151.20 at zoom level 6.
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
-                                                                 zoom:6];
+    // coordinates of Carleton College at zoom level 16.5
+    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:44.461329
+                                                            longitude:-93.155607
+                                                                 zoom:16.5];
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
     mapView_.myLocationEnabled = YES;
     self.view = mapView_;
     
+    //set min and max zoom
+    [mapView_ setMinZoom:14.0f maxZoom:20.0f];
+    
+    /*
     // Creates a marker in the center of the map.
     GMSMarker *marker = [[GMSMarker alloc] init];
     marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
     marker.title = @"Sydney";
     marker.snippet = @"Australia";
-    marker.map = mapView_;
+    marker.map = mapView_;*/
     
     UIButton *trailButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     trailButton.frame = CGRectMake(mapView_.bounds.size.width - 60, mapView_.bounds.size.height - 450, 50, 20);
